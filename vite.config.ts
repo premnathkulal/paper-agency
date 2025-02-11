d27529fc9aac04b1a80453f80925106e18db81b1
Premnath K <premnathkulal1998@gmail.com>
2025-02-11 17:42:41 +0530

    Updated package file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("lodash")) return "lodash";
+            if (id.includes("react")) return "react-vendor";
+            return "vendor"; // General vendor chunk
+          }
+        },
+      },
+    },
+  },
+});
